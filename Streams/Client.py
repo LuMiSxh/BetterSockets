36799889eafc55c6fd7d67bb7856a628de530db7
Drageast <drageast.lp04@gmail.com>
2021-07-11 00:09:31 +0200

    Added asyncio streams and renamed a folder

---
+++ Streams/Client.py
@@ -0,0 +1,33 @@
+import asyncio
+
+
+class WebSocket:
+    def __init__(self, **kwargs):
+        self.__port: int = kwargs.get("port", 8888)
+        self.__ip: str = kwargs.get("ip", "127.0.0.1")
+
+        self.__Reader, self.__Writer = None, None
+        self.__func = kwargs.get("func", None)
+
+
+    def send(self, data):
+        asyncio.run(self.__send(data))
+
+    async def __send(self, data):
+        self.__Reader, self.__Writer = await asyncio.open_connection(self.__ip, self.__port)
+
+        self.__Writer.write(f"{data}".encode())
+        await self.__Writer.drain()
+
+        if self.__func:
+            _data = await self.__Reader.read(100)
+            if _data:
+                retr = self.__func(_data)
+                if retr:
+                    self.__Writer.write(bytes(retr, "utf-8"))
+
+        self.__Writer.close()
+
+
+# Cl = WebSocket()
+# Cl.send("Test")
---
+++ Streams/Server.py
@@ -0,0 +1,39 @@
+import asyncio
+import socket
+
+
+class Server:
+    def __init__(self, **kwargs):
+        self.__port: int = kwargs.get("port", 8888)
+        self.__ip: str = kwargs.get("ip", "127.0.0.1")
+
+        self.__rSocket = socket.socket()
+        self.__Reader, self.__Writer = None, None
+
+        self.__func = kwargs.get("func", None)
+        self.__Server = None
+
+    def listen(self):
+        asyncio.run(self.__listeningPrep())
+
+    async def __listeningPrep(self):
+        self.__Server = await asyncio.start_server(self.__listen, self.__ip, self.__port)
+
+        async with self.__Server:
+            await self.__Server.serve_forever()
+
+    async def __listen(self, reader, writer):
+        data = await reader.read(100)
+        data = data.decode()
+        print(data)
+
+        if self.__func:
+            retr = self.__func(data)
+
+            if retr:
+                writer.write(f"{retr}".encode())
+                await writer.drain()
+
+
+# S = Server()
+# S.listen()
